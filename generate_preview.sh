061d9ca0db2106d7f58d8c99a36a4e327a62871b
0jdxt <jytrn@protonmail.com>
2019-11-14 13:02:01 +0000

    update previews

--- generate_preview.sh
+++ generate_preview.sh
@@ -1,13 +1,21 @@
-#!/bin/zsh
+#!/bin/sh
+
+# NOTE: this script should be run from the wallpapers root directory.
+
+generate_file() {
+	echo "# wallpapers"
+
+	for filename in *.jpg; do
+		printf "\n## %s\n\n![%s](%s)\n" "$filename" "$filename" "$filename"
+	done
+
+	printf "\n## N.B.\nI did not create any of these wallpapers. I did tweak the colors, clean-up noise, and resize most of them. If you are the original creator and would like for me to remove your work, please let me know.\n"
+}
 
 rm README.md
-echo "\n## N.B.\nI did not create any of these wallpapers. I did tweak the colors, clean-up noise, and resize most of them. If you are the original creator and would like for me to remove your work, please let me know.\n" >> README.md
+generate_file >>README.md
 
-echo "# wallpapers" >> README.md
+# NOTE: useful for inspecting readme after creation e.g. `./generate_preview.sh vim`
+[ -n "$1" ] && $1 README.md
 
-for filename in *.jpg;
-do
-  echo "\n## ""$filename""\n![""$filename""](https://github.com/turing753/wallpapers/blob/master/""$filename"")" >> README.md
-done
-
-
+exit 0
