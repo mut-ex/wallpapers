28d5b5a453122ad93aa4ca8abfb144c830b86087
0jdxt <jytrn@protonmail.com>
2019-11-23 21:02:20 +0000

    Changed to conform to shellcheck for sh and markdownlint

--- generate_preview.sh
+++ generate_preview.sh
@@ -2,20 +2,27 @@
 
 # NOTE: this script should be run from the wallpapers root directory.
 
+
+nb_note="""
+## NB
+
+I did not create any of these wallpapers. I did tweak the colors, clean-up noise,
+and resize most of them. If you are the original creator and would like for me
+to remove your work, please let me know."""
+
 generate_file() {
-	echo "# wallpapers"
+    echo "# wallpapers"
 
-	for filename in *.jpg; do
-		printf "\n## %s\n\n![%s](%s)\n" "$filename" "$filename" "$filename"
-	done
+    for filename in *.jpg; do
+        printf "\n## %s\n\n![%s](%s)\n" "$filename" "$filename" "$filename"
+    done
 
-	printf "\n## N.B.\nI did not create any of these wallpapers. I did tweak the colors, clean-up noise, and resize most of them. If you are the original creator and would like for me to remove your work, please let me know.\n"
+    echo "$nb_note"
 }
 
-rm README.md
-generate_file >>README.md
+rm README.md; generate_file >>README.md
 
-# NOTE: useful for inspecting readme after creation e.g. `./generate_preview.sh vim`
+# useful for inspecting readme after creation e.g. $ ./generate_preview.sh vim
 [ -n "$1" ] && $1 README.md
 
 exit 0
